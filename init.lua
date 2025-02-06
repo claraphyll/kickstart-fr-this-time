517f4a1a0807ddcb4370f17c46d8470111083758
mxi <public@complexvector.space>
2025-02-06 13:43:39 +0100

    set up default shift width again

--- init.lua
+++ init.lua
@@ -169,6 +169,11 @@ vim.o.expandtab = true
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Set up default whitespace length
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
