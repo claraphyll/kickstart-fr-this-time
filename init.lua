3f18a1a4e15cba1be5b2f48cc9d9765dfe1cee15
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-03-12 17:24:44 -0400

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -161,6 +161,11 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.expandtab = true
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
